8c554f966d916e426745ea51b484317e28e26b3e
Ted Stresen-Reuter <ted@secret-source.eu>
2020-02-12 20:08:51 +0000

    Add visual feedback on error

--- checkImages.js
+++ checkImages.js
@@ -1,10 +1,17 @@
+
+/**
+ * Determine if the display ratio of an image is the same as its natural ratio
+ * @param {*} img An image element from the source
+ */
 var ratioIsCorrect = (img) => {
     let ratio = img.width / img.height;
     let naturalRatio = img.naturalWidth / img.naturalHeight;
-    if( ratio > 0 && Math.abs(ratio - naturalRatio) > 0.003 ) {
+    if( ratio > 0 && Math.abs(ratio - naturalRatio) >= 0.02 ) {
         // return false;
         console.log(`${ratio - naturalRatio}`);
         console.log({img});
+        img.style.borderWidth = Math.round(img.width * 0.1) + 'px';
+        img.style.borderColor = 'red';
     }
 };
 
